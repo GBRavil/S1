68d2f45c22b9d50d366aa396fce6ccf9deb92691
Ravil <lllx2@yandex.ru>
2022-10-09 15:40:14 +0300

    12

---
+++ Задача 12/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10, 500);
+Console.WriteLine("Число: " + number);
+if(number>99){
+    int b = (number - number % 100) / 100;
+    Console.WriteLine("Третья цифра числа "+b);
+} else {
+    Console.WriteLine("Число двухзначное");
+}
