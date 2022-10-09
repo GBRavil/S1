38a4e471ca21a71df3895d49d36b22f9b31ba37a
Ravil <lllx2@yandex.ru>
2022-10-09 15:29:31 +0300

    11

---
+++ Задача 11/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задайте число");
+String s1 = Console.ReadLine();
+int i1 = Convert.ToInt32(s1);
+Console.WriteLine("Кратно ли числу");
+String s2 = Console.ReadLine();
+int i2 = Convert.ToInt32(s2);
+if(i1%i2==0) {
+    Console.WriteLine(i1 + " кратно " + i2);
+} else {
+    int i3 = i1%i2;
+    Console.WriteLine(i1 + " не кратно " + i2 + " остаток " + i3);
+}
