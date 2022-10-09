e2f6a1ffae9c51757ebb92e001ff0651b8160849
Ravil <lllx2@yandex.ru>
2022-10-09 13:28:19 +0300

    5_6

---
+++ Задача 5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int number = Convert.ToInt32(s);
+for(int i = number*-2; i < number * 2+1; i++) {
+    Console.WriteLine(i);
+}
---
+++ Задача 6/Program.cs
@@ -0,0 +1,6 @@
+﻿int [] number = {1, 2, 3, 4, 5, 6, 7, 8, 9,};
+for(int i = 0; i<number.Length; i++) {
+    if (number[i]% 2 == 0){
+         Console.WriteLine(number[i]);
+    } 
+}
