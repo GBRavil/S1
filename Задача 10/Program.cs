cc7f84f8a084f986b71d3d62beebfab2ed64c36d
Ravil <lllx2@yandex.ru>
2022-10-09 15:08:25 +0300

    10

--- Задача 10/Program.cs
+++ Задача 10/Program.cs
@@ -1,5 +1,6 @@
 ﻿int number = new Random().Next(100, 1000);
 Console.WriteLine("Число: " + number);
-int b = (number % 100-number % 10);
-int c = number - b;
-Console.WriteLine(c);
+// int b = (number % 100-number % 10); обнуляет вторую цифру в числе
+// int c = number - b;
+int b = (number - number % 100) / 100 * 10 +(number % 10); // удаляет вторую цифру в числе
+Console.WriteLine(b);
